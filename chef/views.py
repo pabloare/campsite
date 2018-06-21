b56bb8f5f932ab786277b74db2106e79d72240fe
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-06-21 01:10:22 -0700

    chef

--- chef/views.py
+++ chef/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Chef
+from manager.models import Restaurant
 
-# Create your views here.
+# MAKE CUSTOM USER MODEL FOR CHEF WITH ONLY ID AND RESTAURANT FIELDS
+def home(request):
+    if request.method == 'POST':
+        res_id = request.POST['select-res']
+        chef_id = request.POST['chef_id']
+        res = Restaurant.objects.get(id=res_id)
+        chefs = Chef.objects.filter(restaurant=res)
+        if chefs.filter(chef_id=chef_id).exists():
+
+    return render(request, 'login_chef.html', {'restaurants': Restaurant.objects.all()})
