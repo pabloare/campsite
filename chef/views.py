f647854f11fd6be9ef742012804e4c85139d224f
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-06-26 18:15:00 -0700

    login func

--- chef/views.py
+++ chef/views.py
@@ -6,11 +6,15 @@ from manager.models import Restaurant
 
 # MAKE CUSTOM USER MODEL FOR CHEF WITH ONLY ID AND RESTAURANT FIELDS
 def home(request):
+    error = False
     if request.method == 'POST':
         res_id = request.POST['select-res']
         chef_id = request.POST['chef_id']
         res = Restaurant.objects.get(id=res_id)
         chefs = Chef.objects.filter(restaurant=res)
         if chefs.filter(chef_id=chef_id).exists():
-
-    return render(request, 'login_chef.html', {'restaurants': Restaurant.objects.all()})
+            chef = chefs.get(chef_id=chef_id)
+            return render(request, 'home_chef.html', {'chef': chef})
+        else:
+            error = True
+    return render(request, 'login_chef.html', {'restaurants': Restaurant.objects.all(), 'error': error})
