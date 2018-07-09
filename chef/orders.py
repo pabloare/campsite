9c79a5cb447a3310811e5bb4f91dbef7e2033c08
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-07-09 12:28:50 -0700

    update

--- chef/orders.py
+++ chef/orders.py
@@ -5,8 +5,8 @@ from manager.models import Dish
 
 def render_orders(request, chef_id, res_id):
     chef = Chef.objects.filter(restaurant=res_id).get(chef_id=chef_id)
-    dish = Dish.objects.get(name="California Roll")
-    add_order = Join(chef=chef, dish=dish)
-    add_order.save()
+    # dish = Dish.objects.get(name="California Roll")
+    # add_order = Join(chef=chef, dish=dish)
+    # add_order.save()
     dishes = chef.dishes.all()
     return render(request, 'chef-orders.html', {'dishes': dishes, 'chef': chef.chef_id})
