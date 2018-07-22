dbacce8c6048962ae64a6400c623b3e22c86dd64
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-07-22 11:55:57 -0500

    bug fixes

--- chef/orders.py
+++ chef/orders.py
@@ -12,21 +12,14 @@ def render_orders(request, chef_id, res_id):
     # add_order = Join(chef=chef, dish=dish)
     # add_order.save()
     joins = Join.objects.filter(chef=chef)
-    # for dish in dishes:
-    #     joins = Join.objects.filter(chef=chef, dish=dish)
-    #     for join in joins:
-    #         order = join.order
-    #         order_seats.append(order.seat.seat_number)
-    #         order_tables.append(order.seat.table.table_number)
-    # Join lists in order to iterate in parallel
     return render(request, 'chef-orders.html', {'chef': chef, 'res': res_id, 'joins': joins})
 
-
+## WHEN GOING HERE THE JOIN_ID IS AN INT AND NEEDS TO BE A STR
 def dish_complete(request, join_id):
     join = Join.objects.get(id=join_id)
     join.chef.accumulator -= join.dish.time_to_do
-    chef_id = join.chef.chef_id
-    res_id = join.chef.restaurant.id
+    chef_id = str(join.chef.chef_id)
+    res_id = str(join.chef.restaurant.id)
     join.chef.save()
     join.delete()
     return HttpResponseRedirect('/chef/home/' + chef_id + "/" + res_id)
