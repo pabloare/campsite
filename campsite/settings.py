7cbf683ecdf8f85f2ea92f70f3294b4beb317b5f
Pablo Arellano <pablo.arellanos97@gmail.com>
2018-12-14 13:25:53 -0800

    tly

--- campsite/settings.py
+++ campsite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'hher59g+toe))!i_adv_cr=6bw9f*jo%f0o(^0yp$^d!yms@!k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['campsite-parellano.herokuapp.com', 'www.campsite.ml', '127.0.0.1', '192.168.0.16']
+ALLOWED_HOSTS = ['campsite-parellano.herokuapp.com', 'www.campsite.ml', '127.0.0.1', '192.168.0.16', 'www.treately.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
